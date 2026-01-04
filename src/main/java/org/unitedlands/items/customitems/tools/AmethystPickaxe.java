617f9800ce85b7e6d8b01e328053c9e88acfe2af
Litning11 <dario.mavilia@outlook.com>
2026-01-04 16:40:21 +0000

    Fix amethyst pickaxe method signature preventing item break recognition.

--- src/main/java/org/unitedlands/items/customitems/tools/AmethystPickaxe.java
+++ src/main/java/org/unitedlands/items/customitems/tools/AmethystPickaxe.java
@@ -5,11 +5,13 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
 public class AmethystPickaxe extends CustomTool implements Listener {
 
-    public void handleBlockBreak(Player player, BlockBreakEvent event) {
+    @Override
+    public void handleBlockBreak(Player player, BlockBreakEvent event, EquipmentSlot hand) {
         Block block = event.getBlock();
         if (block.getType() == Material.BUDDING_AMETHYST) {
             block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.BUDDING_AMETHYST));
